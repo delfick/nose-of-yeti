91f25ea155464bdff70ef6a74cba2dd272b762f8
Stephen Moore <stephen@delfick.com>
2012-03-02 21:10:42 +1100

    added tests for test_chooser

---
+++ noseOfYeti/specs/test_chooser_test.py
@@ -0,0 +1,70 @@
+from noseOfYeti.test_chooser import TestChooser
+from should_dsl import *
+
+class Test_TestChooser(object):
+    
+    def setup(self):
+        self.test_chooser = TestChooser()
+    
+    def test_it_resets_done_when_told_about_new_module(self):
+        self.test_chooser.done |should| equal_to({})
+        self.test_chooser.done['a'] = 3
+        self.test_chooser.done |should| equal_to({'a':3})
+        self.test_chooser.new_module()
+        self.test_chooser.done |should| equal_to({})
+        
+    def test_already_visited_puts_kls_name_key_in_done_or_returns_True(self):
+        self.test_chooser.done |should| equal_to({})
+        self.test_chooser.already_visited('a', 'b') |should| be(False)
+        self.test_chooser.done |should| equal_to({'a.b' : True})
+        self.test_chooser.already_visited('a', 'b') |should| be(True)
+        
+        self.test_chooser.already_visited('c', 'd') |should| be(False)
+        self.test_chooser.done |should| equal_to({'a.b' : True, 'c.d' : True})
+        self.test_chooser.already_visited('c', 'd') |should| be(True)
+    
+class Test_TestChooser_Consider(object):
+    
+    def setup(self):
+        self.test_chooser = TestChooser()
+        class TestKlsForTest(object):
+            def ignore_test(self): pass
+            
+            def test_with__test__set(self): pass
+            test_with__test__set.__test__ = False
+            
+            def test_actual(self): pass
+        
+        class TestIgnoredKls(object):
+            def test_things(self): pass
+        
+        class TestKlsWithInherited(TestKlsForTest):
+            def test_on_subclass(self): pass
+            
+        self.TestKlsForTest = TestKlsForTest
+        self.TestIgnoredKls = TestIgnoredKls
+        self.TestKlsWithInherited = TestKlsWithInherited
+    
+    def it_ignores_if_method_starts_with_ignore(self):
+        self.test_chooser.consider(self.TestKlsForTest().ignore_test) |should| be(False)
+    
+    def it_ignores_if_method_has__test__set_to_false(self):
+        self.test_chooser.consider(self.TestKlsForTest().test_with__test__set) |should| be(False)
+    
+    def it_ignores_if_method_has_kls_in_ignoreKls(self):
+        self.test_chooser.consider(self.TestIgnoredKls().test_things) |should| be(True)
+        self.test_chooser.consider(self.TestIgnoredKls().test_things, ignoreKls=['TestIgnoredKls']) |should| be(False)
+        
+    def it_returns_None_if_kls_does_not_have_is_noy_test_set(self):
+        self.test_chooser.consider(self.TestKlsForTest().test_actual) |should| be(None)
+        
+    def it_ignores_inherited_tests_if_is_noy_test_is_set_on_kls(self):
+        self.test_chooser.consider(self.TestKlsWithInherited().test_actual) |should| be(None)
+        self.TestKlsWithInherited.is_noy_spec = True
+        self.test_chooser.consider(self.TestKlsWithInherited().test_actual) |should| be(False)
+        self.test_chooser.consider(self.TestKlsWithInherited().test_on_subclass) |should| be(True)
+        
+    def it_ignores_functions_already_visited(self):
+        self.TestKlsWithInherited.is_noy_spec = True
+        self.test_chooser.consider(self.TestKlsWithInherited().test_actual) |should| be(True)
+        self.test_chooser.consider(self.TestKlsWithInherited().test_actual) |should| be(False)
