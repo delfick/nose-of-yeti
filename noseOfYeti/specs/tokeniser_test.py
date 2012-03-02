488bd274e917a0000ac3fdd182c6729fdfd9a106
Stephen Moore <stephen@delfick.com>
2012-03-02 21:14:42 +1100

    Moving custom matcher from tokeniser_test.py into matchers.py

--- noseOfYeti/specs/tokeniser_test.py
+++ noseOfYeti/specs/tokeniser_test.py
@@ -1,40 +1,7 @@
 """Just run "$nosetests" in the same folder as this file"""
 from noseOfYeti.tokeniser import Tokeniser
 from should_dsl import *
-from tokenize import *
-import cStringIO
-
-########################
-###   CUSTOM MATCHER
-########################
-    
-@matcher
-class ResultIn(object):
-
-    name = 'result_in'
-
-    def __call__(self, radicand):
-        self._radicand = radicand
-        return self
-
-    def match(self, actual):
-        tokeniser, self._actual = actual
-        s = cStringIO.StringIO(self._actual)
-        try:
-            tokens = tokeniser.translate(s.readline)
-            self._expected = untokenize(tokens)
-        finally:
-            s.close()
-            
-        return self._expected == self._radicand
-
-    def message_for_failed_should(self):
-        return 'expected "{0}"\n======================>\n"{1}"\n\n======================$\n"{2}"'.format(
-            *(res.replace(' ', '.').replace('\t', '-') for res in (self._actual, self._radicand, self._expected)))
-
-    def message_for_failed_should_not(self):
-        return 'expected "{0}"\n\tTo not translate to "{1}"'.format(
-            *(res.replace(' ', '.').replace('\t', '-') for res in (self._actual, self._radicand)))
+from matchers import *
 
 ########################
 ###   GENERAL TESTS
