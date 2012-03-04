3450d0d099f609ba3699eace876a5dc63d09867b
Stephen Moore <stephen@delfick.com>
2012-03-04 15:51:35 +1100

    Changing version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup( name='noseOfYeti'
-     , version="0.1"
+     , version="1.0"
      , classifiers=[ 'Intended Audience :: Developers'
                    , 'Programming Language :: Python'
                    , 'Topic :: Software Development :: Documentation'
