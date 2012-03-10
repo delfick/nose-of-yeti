9998967d0c0590d87b57509e535432ad55a1f231
Stephen Moore <stephen@delfick.com>
2012-03-10 20:16:15 +1100

    bumping release version for new sphinx plugin

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup( name = 'noseOfYeti'
-     , version = "1.1"
+     , version = "1.2"
      , classifiers=[ 'Intended Audience :: Developers'
                    , 'Programming Language :: Python'
                    , 'Topic :: Software Development :: Documentation'
