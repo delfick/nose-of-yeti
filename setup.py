d22c32acaab1455768f7e6510a8f4f2309e0e1aa
Stephen Moore <stephen@delfick.com>
2012-03-15 16:03:42 +1100

    version push

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup( name = 'noseOfYeti'
-     , version = "1.4"
+     , version = "1.4.1"
      , classifiers=[ 'Intended Audience :: Developers'
                    , 'Programming Language :: Python'
                    , 'Topic :: Software Development :: Documentation'
