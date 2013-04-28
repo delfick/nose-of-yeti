95060f125e7e2cfd27cc71296e9cefb18e3e8377
Stephen Moore <stephen@delfick.com>
2013-04-28 10:40:02 +1000

    Using config and register_codec function for plugins

--- noseOfYeti/plugins/pylinter.py
+++ noseOfYeti/plugins/pylinter.py
@@ -4,8 +4,24 @@ To be used as a pylint plugin to register the spec encoding
 from pylint.interfaces import IASTNGChecker
 from pylint.checkers import BaseChecker
 
-from noseOfYeti.tokeniser import Tokeniser, TokeniserCodec, determine_imports
-from support import spec_options
+from noseOfYeti.tokeniser.spec_codec import register_from_options
+from noseOfYeti.tokeniser.config import Default
+from support.spec_options import spec_options
+
+import os
+
+def normalise_options(template):
+    env = os.environ
+    parser_options = ['default', 'help', 'type']
+    for option, attributes in template.items():
+        opts = dict((k, v) for k, v in attributes.items() if k in parser_options)
+        opts['default'] = Default(opts['default'](env))
+        yield option, opts
+
+def extract_options(template, options):
+    for option, val in normalise_options(template):
+        name = option.replace('-', '_')
+        yield option, getattr(options, name)
 
 class SpecRegister(BaseChecker):
 
@@ -13,24 +29,12 @@ class SpecRegister(BaseChecker):
     __implements__ = IASTNGChecker
 
     msgs = {'W6969' : ('', '')}
-    options = spec_options.for_pylint()
+    options = dict(normalise_options(spec_options))
     priority = -1
 
     def open(self):
-        imports = determine_imports(
-              extra_imports = ';'.join([d for d in self.config.extra_import if d])
-            , without_should_dsl = self.config.without_should_dsl
-            , with_default_imports = not self.config.no_default_imports
-            )
-
-        tok = Tokeniser(
-              default_kls = self.config.default_kls
-            , import_tokens = imports
-            , wrapped_setup = self.config.wrapped_setup
-            , with_describe_attrs = not self.config.no_describe_attrs
-            )
-
-        TokeniserCodec(tok).register()
+        options = self.config
+        register_from_options(options, spec_options, extractor=extract_options)
 
 def register(linter):
     linter.register_checker(SpecRegister(linter))
