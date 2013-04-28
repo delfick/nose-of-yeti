279b8cf6ac1f1cbd31e36f4266a1cd3e8e213baf
Stephen Moore <stephen@delfick.com>
2013-04-28 23:59:16 +1000

    Making pylinter know the difference between default and user values

--- noseOfYeti/plugins/pylinter.py
+++ noseOfYeti/plugins/pylinter.py
@@ -10,31 +10,56 @@ from support.spec_options import spec_options
 
 import os
 
+class Empty(object): pass
+
 def normalise_options(template):
     env = os.environ
     parser_options = ['default', 'help', 'type']
     for option, attributes in template.items():
         opts = dict((k, v) for k, v in attributes.items() if k in parser_options)
-        opts['default'] = Default(opts['default'](env))
+        opts['default'] = opts['default'](env)
         yield option, opts
 
-def extract_options(template, options):
-    for option, val in normalise_options(template):
-        name = option.replace('-', '_')
-        yield option, getattr(options, name)
+def make_extractor(non_default):
+    """
+        Return us a function to extract options
+        Anything not in non_default is wrapped in a "Default" object
+    """
+    def extract_options(template, options):
+        for option, val in normalise_options(template):
+            name = option.replace('-', '_')
+
+            value = getattr(options, name)
+            if option not in non_default:
+                value = Default(value)
+
+            yield name, value
+    return extract_options
 
 class SpecRegister(BaseChecker):
 
-    name = 'spec_register'
+    name = 'NoyRegister'
     __implements__ = IASTNGChecker
 
     msgs = {'W6969' : ('', '')}
-    options = dict(normalise_options(spec_options))
+    options = list(normalise_options(spec_options))
     priority = -1
+
+    def __init__(self, *args, **kwargs):
+        self.specified = []
+        super(SpecRegister, self).__init__(*args, **kwargs)
+
+    def set_option(self, name, val, action=Empty, opts=Empty):
+        """Determine which options were specified outside of the defaults"""
+        if action is Empty and opts is Empty:
+            self.specified.append(name)
+            super(SpecRegister, self).set_option(name, val)
+        else:
+            super(SpecRegister, self).set_option(name, val, action, opts)
 
     def open(self):
         options = self.config
-        register_from_options(options, spec_options, extractor=extract_options)
+        register_from_options(options, spec_options, extractor=make_extractor(self.specified))
 
 def register(linter):
     linter.register_checker(SpecRegister(linter))
