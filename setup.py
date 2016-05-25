7bf8704da467805f8ad878bbe05655dda3210f29
Stephen Moore <stephen@delfick.com>
2016-05-26 09:03:34 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from textwrap import dedent
 
 setup(
        name = 'noseOfYeti'
-     , version = "1.5.1"
+     , version = "1.5.2"
      , classifiers =
        [ 'Intended Audience :: Developers'
        , 'Programming Language :: Python'
