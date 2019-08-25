57cd2724864c9ac91a391bd6cfda5df02564a113
Stephen Moore <stephen@delfick.com>
2019-08-25 11:35:24 +1000

    Make noseOfYeti place nicely with unittest in pytest

--- noseOfYeti/plugins/pytest.py
+++ noseOfYeti/plugins/pytest.py
@@ -9,6 +9,7 @@ any collected tests that aren't on that class.
 """
 from noseOfYeti.tokeniser.spec_codec import register_from_options
 
+from _pytest.unittest import UnitTestCase
 from functools import partial
 from unittest import mock
 import inspect
@@ -22,36 +23,50 @@ def pytest_pycollect_makeitem(collector, name, obj):
     """Make sure we can have nested noseOfYeti describes"""
     outcome = yield
     res = outcome.get_result()
-    if res is not None:
-        try:
-            isclass = inspect.isclass(res.obj)
-        except Exception:
-            isclass = False
 
-        if not isclass:
-            return res
+    if res is None:
+        return
 
-        outcome.force_result(ModifiedClass(name, parent=collector))
+    if not hasattr(res, "obj"):
+        is_noy_on_obj = getattr(obj, "is_noy_spec", False)
+        is_noy_on_class = getattr(obj.__class__, "is_noy_spec", False)
+        if not is_noy_on_obj and not is_noy_on_class:
+            return
 
-def modified_collect(instance, original_collect):
+    original_item_collect = res.collect
+
+    if isinstance(res, UnitTestCase):
+        original_item_collect = res.collect
+
+        def collect():
+            yield from filter_collection(original_item_collect(), res.obj)
+        mock.patch.object(res, "collect", collect).start()
+    else:
+        def collect():
+            got = original_item_collect()
+            for g in got:
+                original_collect = g.collect
+                mock.patch.object(g, "collect", partial(modified_class_collect, g, original_collect)).start()
+            return got
+
+        mock.patch.object(res, "collect", collect).start()
+
+def modified_class_collect(instance, original_collect):
     instance.session._fixturemanager.parsefactories(instance)
-    collected = original_collect()
-    modified_collected = []
+    yield from filter_collection(original_collect(), instance.obj)
+
+def filter_collection(collected, obj):
     for thing in collected:
         if not isinstance(thing, pytest.Function):
-            modified_collected.append(thing)
+            yield thing
             continue
 
-        is_noy = getattr(instance.obj.__class__, "is_noy_spec", False)
-        if not is_noy or thing.obj.__name__ in instance.obj.__class__.__dict__:
-            modified_collected.append(thing)
+        try:
+            isclass = inspect.isclass(obj)
+            if not isclass:
+                obj = obj.__class__
+        except Exception:
+            pass
 
-    return modified_collected
-
-class ModifiedClass(pytest.Class):
-    def collect(self):
-        got = super().collect()
-        for g in got:
-            original_collect = g.collect
-            mock.patch.object(g, "collect", partial(modified_collect, g, original_collect)).start()
-        return got
+        if thing.obj.__name__ in obj.__dict__:
+            yield thing
