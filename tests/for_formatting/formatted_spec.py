dfbcdaa8c498a0f26189382ad47d71ab88625584
Stephen Moore <stephen@delfick.com>
2022-11-17 21:59:59 +1100

    test: Adding to format test to ensure comments are correct

--- tests/for_formatting/formatted_spec.py
+++ tests/for_formatting/formatted_spec.py
@@ -1,9 +1,17 @@
 # coding: spec
 
 
+def awesome(a: str) -> bool:
+    return True
+
+
+def hi(twos: int, word: str, b: bool):
+    pass
+
+
 describe "TestThing":
     async it "totes works yo", one: int, three: str:
-        assert awesome()
+        assert awesome(2)  # type: ignore
 
 it "is great":
     assert False, "or is it ?"
