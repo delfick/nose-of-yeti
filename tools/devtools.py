cf8d909f81ea01d43189d3dc87bd717eb90bb790
Stephen Moore <stephen@delfick.com>
2023-11-08 16:55:48 +1100

    dev: update tooling

--- tools/devtools.py
+++ tools/devtools.py
@@ -1,46 +1,33 @@
-from pathlib import Path
-import typing as tp
-import socketserver
-import http.server
-import webbrowser
-import threading
-import platform
 import inspect
-import socket
-import shutil
-import shlex
-import time
-import sys
 import os
+import platform
+import shlex
+import sys
+import typing as tp
+from collections.abc import Callable
+from pathlib import Path
 
 here = Path(__file__).parent
 
 if platform.system() == "Windows":
-    import mslex
+    import mslex  # type:ignore[import]
 
     shlex = mslex  # noqa
-
-if sys.version_info < (3, 10):
-    Dict = tp.Dict
-    List = tp.List
-else:
-    Dict = dict
-    List = list
 
 
 class Command:
     __is_command__: bool
 
-    def __call__(self, bin_dir: Path, args: List[str]) -> None:
+    def __call__(self, bin_dir: Path, args: list[str]) -> None:
         ...
 
 
-def command(func: tp.Callable) -> tp.Callable:
+def command(func: Callable) -> Callable:
     tp.cast(Command, func).__is_command__ = True
     return func
 
 
-def run(*args: tp.Union[str, Path], _env: tp.Optional[Dict[str, str]] = None) -> None:
+def run(*args: str | Path) -> None:
     cmd = " ".join(shlex.quote(str(part)) for part in args)
     print(f"Running '{cmd}'")
     ret = os.system(cmd)
@@ -49,7 +36,7 @@ def run(*args: tp.Union[str, Path], _env: tp.Optional[Dict[str, str]] = None) ->
 
 
 class App:
-    commands: Dict[str, Command]
+    commands: dict[str, Command]
 
     def __init__(self):
         self.commands = {}
@@ -64,7 +51,7 @@ class App:
                 ), f"Expected '{name}' to have correct signature, have {inspect.signature(val)} instead of {compare}"
                 self.commands[name] = val
 
-    def __call__(self, args: List[str]) -> None:
+    def __call__(self, args: list[str]) -> None:
         bin_dir = Path(sys.executable).parent
 
         if args and args[0] in self.commands:
@@ -75,66 +62,54 @@ class App:
         sys.exit(f"Unknown command:\nAvailable: {sorted(self.commands)}\nWanted: {args}")
 
     @command
-    def format(self, bin_dir: Path, args: List[str]) -> None:
+    def format(self, bin_dir: Path, args: list[str]) -> None:
         if not args:
             args = [".", *args]
         run(bin_dir / "black", *args)
 
     @command
-    def lint(self, bin_dir: Path, args: List[str]) -> None:
+    def lint(self, bin_dir: Path, args: list[str]) -> None:
         run(bin_dir / "pylama", *args)
 
     @command
-    def tests(self, bin_dir: Path, args: List[str]) -> None:
+    def tests(self, bin_dir: Path, args: list[str]) -> None:
         if "-q" not in args:
             args = ["-q", *args]
-        run(bin_dir / "pytest", *args, _env={"NOSE_OF_YETI_BLACK_COMPAT": "false"})
 
-    @command
-    def tox(self, bin_dir: Path, args: List[str]) -> None:
-        run(bin_dir / "tox", *args)
+        env = os.environ
+        env["NOSE_OF_YETI_BLACK_COMPAT"] = "false"
 
-    @command
-    def docs(self, bin_dir: Path, args: List[str]) -> None:
-        do_view: bool = False
-        docs_path = here / ".." / "docs"
-        for arg in args:
-            if arg == "fresh":
-                build_path = docs_path / "_build"
-                if build_path.exists():
-                    shutil.rmtree(build_path)
-            elif arg == "view":
-                do_view = True
-
-        os.chdir(docs_path)
-        run(bin_dir / "sphinx-build", "-b", "html", ".", "_build/html", "-d", "_build/doctrees")
-
-        if do_view:
-
-            with socket.socket() as s:
-                s.bind(("", 0))
-                port = s.getsockname()[1]
-
-            address = f"http://127.0.0.1:{port}"
-            results = docs_path / "_build" / "html"
-
-            class Handler(http.server.SimpleHTTPRequestHandler):
-                def __init__(self, *args, **kwargs):
-                    super().__init__(*args, directory=str(results), **kwargs)
-
-            def open_browser():
-                time.sleep(0.2)
-                webbrowser.open(address)
-
-            with socketserver.TCPServer(("", port), Handler) as httpd:
-                print(f"Serving docs at {address}")
-                thread = threading.Thread(target=open_browser)
-                thread.daemon = True
-                thread.start()
+        files: list[str] = []
+        if "TESTS_CHDIR" in env:
+            ags: list[str] = []
+            test_dir = Path(env["TESTS_CHDIR"]).absolute()
+            for a in args:
+                test_name = ""
+                if "::" in a:
+                    filename, test_name = a.split("::", 1)
+                else:
+                    filename = a
                 try:
-                    httpd.serve_forever()
-                except KeyboardInterrupt:
-                    pass
+                    p = Path(filename).absolute()
+                except:
+                    ags.append(a)
+                else:
+                    if p.exists():
+                        rel = p.relative_to(test_dir)
+                        if test_name:
+                            files.append(f"{rel}::{test_name}")
+                        else:
+                            files.append(str(rel))
+                    else:
+                        ags.append(a)
+            args = ags
+            os.chdir(test_dir)
+
+        run(bin_dir / "pytest", *files, *args)
+
+    @command
+    def tox(self, bin_dir: Path, args: list[str]) -> None:
+        run(bin_dir / "tox", *args)
 
 
 app = App()
